3ae296928a239b21a2072383686a2bfc4b08f660
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2023-07-14 11:10:14 -0400

    Simplified scanBasePackages.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication(scanBasePackages = {"org.example.Enum", "org.example", "org.example.Objects", "org.example.Stores"})
+@SpringBootApplication(scanBasePackages = "org.example")
 public class Main {
     public static void main(final String[] args) {
         SpringApplication.run(Main.class, args);
